a4e68d3ef5cad4cf9564386d50d7657c8d2ab32d
Yifan Ning <pharmgx@gmail.com>
2017-05-25 15:16:17 -0400

    add sql query for getting MP claim information

--- src/main/resources/resources/mpevidence/sql/getClaimByMethodAndDrug.sql
+++ src/main/resources/resources/mpevidence/sql/getClaimByMethodAndDrug.sql
@@ -2,14 +2,14 @@ WITH method AS (
 SELECT mp_claim_id, mp_data_index, m.entered_value as method from ohdsi.method m),
 
 s AS (
-SELECT ca.id, cb.claim_text, q.qvalue, q.concept_code, q.vocabulary_id
+SELECT ca.id, cb.claim_text, q.qvalue, q.concept_code, q.vocabulary_id, q.qualifier_role_concept_code, q.qualifier_role_vocabulary_id
 FROM ohdsi.mp_claim_annotation ca 
 JOIN ohdsi.oa_claim_body cb on cb.is_oa_body_of = ca.id
 JOIN ohdsi.qualifier q on q.claim_body_id = cb.id
 WHERE q.subject = True),
 
 o AS (
-SELECT ca.id, q.qvalue, q.concept_code, q.vocabulary_id
+SELECT ca.id, q.qvalue, q.concept_code, q.vocabulary_id, q.qualifier_role_concept_code, q.qualifier_role_vocabulary_id
 FROM ohdsi.mp_claim_annotation ca 
 JOIN ohdsi.oa_claim_body cb on cb.is_oa_body_of = ca.id
 JOIN ohdsi.qualifier q on q.claim_body_id = cb.id
@@ -22,7 +22,7 @@ JOIN ohdsi.oa_claim_body cb on cb.is_oa_body_of = ca.id
 JOIN ohdsi.qualifier q on q.claim_body_id = cb.id
 WHERE q.predicate = True)
 
-SELECT method.mp_claim_id, method.mp_data_index, s.claim_text, method.method, s.qvalue as subject, p.qvalue as relationship, o.qvalue as object
+SELECT method.mp_claim_id, method.mp_data_index, s.claim_text, method.method, s.qvalue as subject, p.qvalue as relationship, o.qvalue as object, s.qualifier_role_concept_code, o.qualifier_role_concept_code
 from method
 JOIN s ON s.id = method.mp_claim_id
 JOIN o ON o.id = method.mp_claim_id
