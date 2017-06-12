181d2ca836f41623373de8be433faa4492364eb2
Yifan Ning <pharmgx@gmail.com>
2017-06-12 12:52:27 -0400

    standardize drug names for query method

--- src/main/resources/resources/mpevidence/sql/getMethodByDrugNames.sql
+++ src/main/resources/resources/mpevidence/sql/getMethodByDrugNames.sql
@@ -1,21 +1,21 @@
 WITH method AS (
 SELECT mp_claim_id, mp_data_index, m.entered_value, m.inferred_value from ohdsi.method m),
-p AS (
-SELECT ca.id, cb.claim_text, q.qvalue, q.concept_code, q.vocabulary_id, q.qualifier_role_concept_code, cb.label
+s AS (
+SELECT ca.id, c.concept_name, q.concept_code, q.vocabulary_id, q.qualifier_role_concept_code, cb.label
 FROM ohdsi.mp_claim_annotation ca 
 JOIN ohdsi.oa_claim_body cb on cb.is_oa_body_of = ca.id
-JOIN ohdsi.qualifier q on q.claim_body_id = cb.id
-WHERE q.subject = True),
+JOIN ohdsi.qualifier q on q.claim_body_id = cb.id AND q.subject = True AND q.concept_code!= '' AND q.vocabulary_id = '44819104'
+JOIN public.concept c ON c.concept_code = q.concept_code),
 o AS (
-SELECT ca.id, q.qvalue, q.concept_code, q.vocabulary_id, q.qualifier_role_concept_code, cb.label
+SELECT ca.id, c.concept_name, q.concept_code, q.vocabulary_id, q.qualifier_role_concept_code, cb.label
 FROM ohdsi.mp_claim_annotation ca 
 JOIN ohdsi.oa_claim_body cb on cb.is_oa_body_of = ca.id
-JOIN ohdsi.qualifier q on q.claim_body_id = cb.id
-WHERE q.object = True)
+JOIN ohdsi.qualifier q on q.claim_body_id = cb.id AND q.object = True AND q.concept_code!= '' AND q.vocabulary_id = '44819104'
+JOIN public.concept c ON c.concept_code = q.concept_code)
 SELECT method.entered_value, method.inferred_value, count(*) as cnts
 from method
-JOIN p ON p.id = method.mp_claim_id
+JOIN s ON s.id = method.mp_claim_id
 JOIN o ON o.id = method.mp_claim_id
-WHERE (o.qvalue = '@drugname1' AND p.qvalue = '@drugname2')
-OR (p.qvalue = '@drugname1' AND o.qvalue = '@drugname2')
+WHERE (o.concept_name = '@drugname1' AND s.concept_name = '@drugname2')
+OR (s.concept_name = '@drugname2' AND o.concept_name = '@drugname1')
 GROUP BY method.entered_value, method.inferred_value;
